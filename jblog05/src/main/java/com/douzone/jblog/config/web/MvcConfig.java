6756f03d7c91a04bbae88a3749529a7a5ba725e2
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-02-14 17:29:09 +0900

    jblog05 - web.xml(x), static resource handler mapping

--- jblog05/src/main/java/com/douzone/jblog/config/web/MvcConfig.java
+++ jblog05/src/main/java/com/douzone/jblog/config/web/MvcConfig.java
@@ -1,11 +1,15 @@
 package com.douzone.jblog.config.web;
 
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -24,7 +28,14 @@ public class MvcConfig implements WebMvcConfigurer{
 	}
 
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/assets/**")
+			.addResourceLocations("classpath:/assets/");
 	}
+	
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
 }
